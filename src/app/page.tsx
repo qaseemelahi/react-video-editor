73535fa0a8a5f8ac1f935befdd6593b2b21ad573
qaseem <qaseemelahi@gmail.com>
2024-01-02 13:19:58 +0500

    implement init code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,18 @@
-export default function Home() {
+'use client';
+
+import dynamic from 'next/dynamic'
+
+const DynmicEditor = dynamic(() => import('../components/Editor').then(a => a.EditorWithStore), {
+  ssr: false,
+})
+
+
+function EditorPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <a
-        href="/editor"
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        rel="noopener noreferrer"
-      >
-        <h2 className={`mb-3 text-2xl font-semibold`}>
-          Go To Editor{" "}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
-          </span>
-        </h2>
-        <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          Created By Amit Digga
-        </p>
-      </a>
-    </main>
+    <DynmicEditor />
   );
 }
+
+EditorPage.diplsayName = "EditorPage";
+
+export default EditorPage;
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -49,9 +49,6 @@ export const Editor = observer(() => {
   }, []);
   return (
     <div className="grid grid-rows-[20px_500px_1fr] grid-cols-[60px_200px_800px_1fr] h-[100%]">
-      <div className="col-span-4 bg-slate-300 text-right px-2 text-xs">
-        Created By Amit Digga
-      </div>
       <div className="tile row-span-2 flex flex-col">
         <Menu />
       </div>
